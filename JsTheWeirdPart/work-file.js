765aff286ad62f82b91f5870261a4bc6e8c4db12
Samuele Calamita <samuele.calamita@pinuts.de>
2018-06-25 10:05:06 +0200

    vanilla js nav

--- JsTheWeirdPart/work-file.js
+++ JsTheWeirdPart/work-file.js
@@ -1,7 +1,13 @@
-function Navigation(open){
-  this.open = open;
-}
+var toggle = document.querySelectorAll('.js-menu-toggle');
 
-Navigation.prototype.openFunction = function() {
-  return this.classList.add('is-open')
+for(var i=0; i<toggle.length; i++){
+
+  toggle[i].addEventListener('click', function(e){
+
+    e.preventDefault();
+    toggle[i].nextElementSibling.classList.remove('is-visible');
+    this.nextElementSibling.classList.add('is-visible');
+
+  });
+
 }
